fd44f7f443f1b600da15a9f4abfca6e418dff71e
mostapha khalid <mustafaelkhokhy@gamil.com>
2025-06-24 18:53:16 +0300

    handle page

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,16 @@ Route::get('/', function () {
 Route::get('index', function () {
     return view("index");
 });
+Route::get('post', function () {
+    return view("post");
+});
+Route::get('contact', function () {
+    return view("contact");
+});
+Route::get('about', function () {
+    return view("about");
+});
+
 
 
 /***************** TEST Route **************************** */
